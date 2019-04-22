327d42c16cdb5769fcc988d020966814c7673b37
Rendyansyah Syabany <rendysyabany@gmail.com>
2019-04-22 08:23:44 +0700

    add nextjs

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// function Home() {
+//     return <div>Welcome to Next.js!</div>;
+//   }
+  
+//   export default Home;
+
+const Index = () => (
+    <div>
+        <p>Welcome to Next.js!!</p>
+    </div>
+)
+
+export default Index;
