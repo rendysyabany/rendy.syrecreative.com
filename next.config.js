61ed10f96d24092eda54927ef0f753a11c874120
Rendyansyah Syabany <rendysyabany@gmail.com>
2019-05-04 18:34:17 +0700

    update config 03

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     target: 'serverless',
-    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://github.com/rendysyabany/rendy.syrecreative.com.git' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/rendy.syrecreative.com' : '',
 }
