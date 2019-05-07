8e97cff13d21be698c8e5a07e549e2a7169144bf
Rendyansyah Syabany <rendysyabany@gmail.com>
2019-05-07 08:28:44 +0700

    add new

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// function Home() {
+//     return <div>Welcome to Next.js!</div>;
+//   }
+  
+//   export default Home;
+
+const Index = () => (
+    <div>
+        <p>Ongoing Development</p>
+    </div>
+)
+
+export default Index;
